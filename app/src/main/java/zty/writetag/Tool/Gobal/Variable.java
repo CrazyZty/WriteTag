5c8e49abdd25c06a42776d898aad8c23c90e98e1
zhengtongyu <2273653372@qq.com>
2016-06-22 15:24:10 +0800

    更新

--- app/src/main/java/zty/writetag/Tool/Gobal/Variable.java
+++
@@ -1,9 +0,0 @@
-package zty.writetag.Tool.Gobal;
-
-/**
- * Created by 郑童宇 on 2016/05/24.
- */
-public class Variable {
-    public static String StorageDirectoryPath;
-    public static String ErrorFilePath;
-}
